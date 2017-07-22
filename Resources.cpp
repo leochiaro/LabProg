8cacde280a5cc32778be0b2867945c91db1b84d7
Leonardo <leonardo.chiarini97@gmail.com>
2017-07-22 08:50:20 +0200

    aggiunte classe Resources, subject concreto

---
+++ Resources.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leonardo on 22/07/2017.
+//
+
+#include "Resources.h"
---
+++ Resources.h
@@ -0,0 +1,14 @@
+//
+// Created by Leonardo on 22/07/2017.
+//
+
+#ifndef PROVAQT_RESOURCES_H
+#define PROVAQT_RESOURCES_H
+
+
+class Resources {
+
+};
+
+
+#endif //PROVAQT_RESOURCES_H
